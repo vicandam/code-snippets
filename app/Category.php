70ff0e56f0f1b004928ba331666289e78e70d76e
vic101 <vicandam01@gmail.com>
2019-10-11 15:21:36 +0800

    - added backend integration

---
+++ app/Category.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Validator;
+
+class Category extends Model
+{
+    public function topic()
+    {
+        return $this->hasMany(Topic::class);
+    }
+
+    public function validate($input)
+    {
+        $validator = Validator::make($input, [
+            'category' => 'required',
+            'photo' => 'required',
+        ]);
+
+        return $validator->validate();
+    }
+
+    public function scopeFilterSearch($query, $input)
+    {
+        $keyword = ! empty($input['keyword'])    ? $input['keyword']  : null;
+
+        if ($keyword) {
+            $query = $query->where('name', 'like', '%' . $keyword . '%');
+        }
+
+        return $query;
+    }
+}
