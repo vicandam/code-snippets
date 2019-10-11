3ac72a81f243913488223879d15d6e7c38ee52f1
Rey Obejero <rey.obejero@gmail.com>
2019-10-11 16:36:49 +0800

    -ui changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,56 +82,59 @@
                     >
 
                         <template>
-                            <v-card
-                                class="mx-auto"
-                                color="#EFEBE9"
-                                light
-                                tile
-                                flat
-                            >
-                                <v-card-title>
-                                    <v-avatar size="50px" class="mr-2">
-                                        <img
-                                            src="https://cdn.vuetifyjs.com/images/john.jpg"
-                                            alt="John"
-                                        >
-                                    </v-avatar>
-                                    <span class="headline">Php</span>
-                                </v-card-title>
+                            <a href="{{ route('login') }}"
+                               class="v-btn v-btn--flat v-btn--text">
+                                <v-card
+                                    class="mx-auto"
+                                    color="#EFEBE9"
+                                    light
+                                    tile
+                                    flat
+                                >
+                                    <v-card-title>
+                                        <v-avatar size="50px" class="mr-2">
+                                            <img
+                                                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                                                alt="John"
+                                            >
+                                        </v-avatar>
+                                        <span class="headline">Php</span>
+                                    </v-card-title>
 
-                                <v-card-text class="title">
-                                    "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
-                                </v-card-text>
+                                    <v-card-text class="title">
+                                        "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+                                    </v-card-text>
 
-                                <v-card-actions>
-                                    <v-list-item class="grow">
-                                        <v-list-item-avatar color="grey darken-3">
-                                            <v-img
-                                                class="elevation-6"
-                                                src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                                            ></v-img>
-                                        </v-list-item-avatar>
+                                    <v-card-actions>
+                                        <v-list-item class="grow">
+                                            <v-list-item-avatar color="grey darken-3">
+                                                <v-img
+                                                    class="elevation-6"
+                                                    src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                                                ></v-img>
+                                            </v-list-item-avatar>
 
-                                        <v-list-item-content>
-                                            <v-list-item-title>Created 2 months ago by vic</v-list-item-title>
-                                        </v-list-item-content>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Created 2 months ago by vic</v-list-item-title>
+                                            </v-list-item-content>
 
-                                        <v-row
-                                            align="center"
-                                            justify="end"
-                                        >
-                                            <v-icon class="mr-1">mdi-heart</v-icon>
-                                            <span class="subheading mr-2">256</span>
-                                            <span class="mr-1">·</span>
-                                            <v-icon class="mr-1">remove_red_eye</v-icon>
-                                            <span class="subheading">45</span>
-                                            <span class="mr-1">·</span>
-                                            <v-icon class="mr-1">mdi-share-variant</v-icon>
-                                            <span class="subheading">45</span>
-                                        </v-row>
-                                    </v-list-item>
-                                </v-card-actions>
-                            </v-card>
+                                            <v-row
+                                                align="center"
+                                                justify="end"
+                                            >
+                                                <v-icon class="mr-1">mdi-heart</v-icon>
+                                                <span class="subheading mr-2">256</span>
+                                                <span class="mr-1">·</span>
+                                                <v-icon class="mr-1">remove_red_eye</v-icon>
+                                                <span class="subheading">45</span>
+                                                <span class="mr-1">·</span>
+                                                <v-icon class="mr-1">mdi-share-variant</v-icon>
+                                                <span class="subheading">45</span>
+                                            </v-row>
+                                        </v-list-item>
+                                    </v-card-actions>
+                                </v-card>
+                            </a>
                         </template>
 
                         <template>
