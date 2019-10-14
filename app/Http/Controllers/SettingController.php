57f4b511c754455d69c29e2c0261593bb40ad8e9
vic101 <vicandam01@gmail.com>
2019-10-14 14:38:13 +0800

    - added topic controller middleware

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
     public function index()
     {
         $user = User::find(auth()->id());
