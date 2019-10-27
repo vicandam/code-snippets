3f3fc1612733e7ff474295cc2ae5c0178804b6d0
vic101 <vicandam01@gmail.com>
2019-10-27 19:20:52 +0800

    - show only non admin users in users list

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::where('super_user', 0)->get();
 
         $result = [
             'message' => 'Successfully retrieved users.',
